3d9af368d06e35726ce6cae0dd23bcc2a22151e2
thyroxin <penetratatsu@gmail.com>
2024-11-15 02:42:23 +0900

    ファイルまだ分けてない

--- server.rb
+++ server.rb
@@ -3,29 +3,89 @@ require 'json'
 # サーバーの設定
 server = WEBrick::HTTPServer.new(Port: 8080)
 
-# サーブレットを作成
-class GoodbyeServlet < WEBrick::HTTPServlet::AbstractServlet
-  def do_POST(request, response)
-    response.status = 200
-    response['Content-Type'] = 'application/json'
-
-    post_data = JSON.parse(request.body)
-
-    # ここで受け取ったデータを処理する
-    response.body = post_data.to_json
-  end
-end
-
-
-# 新しいサーブレットを作成
-class HelloServlet < WEBrick::HTTPServlet::AbstractServlet
+# "/"
+class HomeServlet < WEBrick::HTTPServlet::AbstractServlet
   def do_GET(request, response)
     response.status = 200
     response['Content-Type'] = 'text/html; charset=utf-8'
     response.body = File.open("./html/index.html").read
-    ## 
   end
 end
+
+server.mount('/', HomeServlet)
+
+# "/stage_0"
+class StageZeroServlet < WEBrick::HTTPServlet::AbstractServlet
+  def do_GET(request, response)
+    response.status = 200
+    response['Content-Type'] = 'text/html; charset=utf-8'
+    response.body = File.open("./html/stage_0/index.html").read
+  end
+end
+
+server.mount('/stage_0', StageZeroServlet)
+
+class StageZeroGetServlet < WEBrick::HTTPServlet::AbstractServlet
+  def do_GET(request, response)
+    response.status = 404
+    response['Content-Type'] = ''
+    response.body = File.open("./html/index.html").read
+  end
+end
+
+server.mount('/stage_0/get', StageZeroGetServlet)
+
+# "/stage_1"
+class StageOneServlet < WEBrick::HTTPServlet::AbstractServlet
+  def do_GET(request, response)
+    response.status = 200
+    response['Content-Type'] = 'text/html; charset=utf-8'
+    response.body = File.open("./html/stage_1/index.html").read
+  end
+end
+
+server.mount('/stage_1', StageOneServlet)
+
+class StageOneRedirectFromServlet < WEBrick::HTTPServlet::AbstractServlet
+  def do_GET(request, response)
+    response.status = 200
+    response['Content-Type'] = 'text/html; charset=utf-8'
+    response.body = File.open("./html/stage_1/redirect_from/index.html").read
+  end
+end
+
+server.mount('/stage_1/redirect_from', StageOneRedirectFromServlet)
+
+class StageOneRedirectToServlet < WEBrick::HTTPServlet::AbstractServlet
+  def do_GET(request, response)
+    response.status = 200
+    response['Content-Type'] = 'text/html; charset=utf-8'
+    response.body = File.open("./html/stage_1/redirect_to/index.html").read
+  end
+end
+
+server.mount('/stage_1/redirect_to', StageOneRedirectToServlet)
+
+# "/stage_2"
+class StageTwoServlet < WEBrick::HTTPServlet::AbstractServlet
+  def do_GET(request, response)
+    response.status = 200
+    response['Content-Type'] = 'text/html; charset=utf-8'
+    response.body = File.open("./html/stage_2/index.html").read
+  end
+end
+
+server.mount('/stage_2', StageTwoServlet)
+
+class StageTwoPostServlet < WEBrick::HTTPServlet::AbstractServlet
+  def do_POST(request, response)
+    response.status = 200
+    response['Content-Type'] = 'text/html; charset=utf-8'
+    response.body = File.open("./html/stage_2/post/index.html").read
+  end
+end
+
+server.mount('/stage_2/post', StageTwoPostServlet)
 
 # 新しいサーブレットを作成
 class ImageServlet < WEBrick::HTTPServlet::AbstractServlet
@@ -36,11 +96,8 @@ class ImageServlet < WEBrick::HTTPServlet::AbstractServlet
   end
 end
 
-# 既存のエンドポイントにサーブレットをマウント
-server.mount('/', GoodbyeServlet)
+server.mount('/image', ImageServlet)
 
-# 新しいエンドポイントにサーブレットをマウント
-server.mount('/', HelloServlet)
 
 # Ctrl+Cでサーバーを停止するためのシグナルハンドラを設定
 trap('INT') { server.shutdown }
